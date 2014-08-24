b38aaf28ea2cb03d4dbcbdef2efa4f1fae71c920
Atom <fromatom@gmail.com>
2014-08-24 21:19:11 +0900

    Fix bug

--- pukiwiki-watch-men.rb
+++ pukiwiki-watch-men.rb
@@ -17,7 +17,7 @@ class PukiWikiRssReader
     end
 
     uri = URI.parse( ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
     @rss = nil
     begin
