c6fcb098806deeade9001f0e92774e69ea17643c
Atom <fromatom@gmail.com>
2014-08-24 21:09:05 +0900

    Debug

--- pukiwiki-watch-men.rb
+++ pukiwiki-watch-men.rb
@@ -16,12 +16,8 @@ class PukiWikiRssReader
       f.read
     end
 
-    if ENV["REDISTOGO_URL"] != nil
-      uri = URI.parse(ENV["REDISTOGO_URL"])
-      redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    else
-      redis = Redis.new host:"127.0.0.1", port:"6379"
-    end
+    uri = URI.parse( ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
     @rss = nil
     begin
